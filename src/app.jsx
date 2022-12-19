4018e599699c5fdb1fc93b3f870aef06e203a03b
HeadInoueTakuma <inoue.takuma@pro.headwaters.co.jp>
2022-12-19 23:09:23 +0900

    create btn

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// ボタンを押した時にアラートを表示する
+
+// 関数を使って画面の要素（コンポーネント）を表現する
+const App = () => {
+  return (
+    <>
+      <h1>test h1</h1>
+      <p>test p</p>
+      <button>ボタン</button>
+    </>
+  );
+};
+
+// Appコンポーネントを他のファイルからでも使用できるように宣言
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
+// コンポーネントAppを使用できるようにインポートする
+import App from "./app.jsx";
 
-// 関数を使って画面の要素（コンポーネント）を表現する
-
-// 引数を受け取らず、戻り値でnullを返すApp関数を作成
-const App = () => {
-  return <h1>test</h1>;
-};
-
-// Appコンポーネントを画面に反映する
+// コンポーネント化したAppコンポーネントを画面に反映する
 // コンポーネントを画面に反映するためにはReactDom（ライブラリ）の中のRender関数を使用する
-ReactDom.render(<App />, document.getElementById("root"));// index.htmlのrootタグに描画
+ReactDom.render(<App />, document.getElementById("root")); // index.htmlのrootタグに描画
